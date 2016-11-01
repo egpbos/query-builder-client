68261cc13f212e6889c46f8a7e0f8187a0f57f4e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-11-01 15:57:37 +0100

    simplified render method by moving some stuff to its own method

--- src/components/node-render.tsx
+++ src/components/node-render.tsx
@@ -14,26 +14,35 @@ export default class NodeRender extends React.Component<props, state> {
     }
 
 
-    public render() {
+    public determineNodeClass(node: Node): string {
 
-        let indent: number = Node.paddingPerLevel * this.props.node.level;
-        let style = {'paddingLeft': indent.toString() + 'px'};
-        let classes = 'node ';
-        if (this.props.node.isinstance) {
-            classes += 'instance';
-        } else if (this.props.node.isentity) {
-            classes += 'entity';
+        let nodeclass = '';
+        if (node.isinstance) {
+            nodeclass += 'instance';
+        } else if (node.isentity) {
+            nodeclass += 'entity';
         } else {
             throw new Error('Unknown clause');
         }
+        return nodeclass;
+    }
+
+
+    public render() {
+
+        let node = this.props.node;
+
+        let indent = {'paddingLeft': (Node.paddingPerLevel * node.level).toString() + 'px'};
+
+        let nodeclass = this.determineNodeClass(node);
 
         let tsx = (
-            <div className={classes} style={style}>
-                <div className='node-bullet'>
-                    {this.props.node.isexpandable ? '+' : '\u2022'}
+            <div className={nodeclass} style={indent}>
+                <div className='bullet' onClick={() => node.onClickHandler(node.name)}>
+                    {node.isexpandable ? '+' : '\u2022'}
                 </div>
-                <div className='node-content' >
-                    {this.props.node.name}
+                <div className='content' >
+                    {node.name}
                 </div>
             </div>
         );
