1bcf361c89f201dcd3fdbd4121132aa3989bde4c
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-11-01 15:55:22 +0100

    updated the test data

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,15 @@ import Node            from './components/node';
 
 
 let nodes = [
-    new Node({isinstance: false, isentity: true, name: 'firstnode', level: 0, isexpandable: true}),
-    new Node({isinstance: false, isentity: true, name: 'secondnode', level: 0, isexpandable: true}),
-    new Node({isinstance: true, isentity: false, name: 'thirdnode', level: 1, isexpandable: false}),
-    new Node({isinstance: true, isentity: false, name: 'fourthnode', level: 1, isexpandable: false}),
-    new Node({isinstance: true, isentity: false, name: 'fifthnode', level: 1, isexpandable: false}),
-    new Node({isinstance: false, isentity: true, name: 'sixthnode', level: 0, isexpandable: true}),
-    new Node({isinstance: false, isentity: true, name: 'seventhnode', level: 1, isexpandable: true}),
-    new Node({isinstance: false, isentity: true, name: 'eightnode', level: 2, isexpandable: false}),
-    new Node({isinstance: true, isentity: false, name: 'ninthnode', level: 2, isexpandable: false})
+    new Node({isinstance: false, isentity: true, name: 'firstnode', level: 0, isexpandable: true, isexpanded: false}),
+    new Node({isinstance: false, isentity: true, name: 'secondnode', level: 0, isexpandable: true, isexpanded: true}),
+    new Node({isinstance: true, isentity: false, name: 'thirdnode', level: 1, isexpandable: false, isexpanded: false}),
+    new Node({isinstance: true, isentity: false, name: 'fourthnode', level: 1, isexpandable: false, isexpanded: false}),
+    new Node({isinstance: true, isentity: false, name: 'fifthnode', level: 1, isexpandable: false, isexpanded: false}),
+    new Node({isinstance: false, isentity: true, name: 'sixthnode', level: 0, isexpandable: true, isexpanded: true}),
+    new Node({isinstance: false, isentity: true, name: 'seventhnode', level: 1, isexpandable: true, isexpanded: true}),
+    new Node({isinstance: false, isentity: true, name: 'eightnode', level: 2, isexpandable: false, isexpanded: false}),
+    new Node({isinstance: true, isentity: false, name: 'ninthnode', level: 2, isexpandable: false, isexpanded: false})
 ];
 
 
