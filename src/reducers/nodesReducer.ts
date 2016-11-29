a8ae9662032aae842a67699295ac360c479811a1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-11-29 14:41:36 +0100

    fetching of child nodes now almost works. Probably need to use something like redux-thunk (see issue #14)

--- src/reducers/nodesReducer.ts
+++ src/reducers/nodesReducer.ts
@@ -1,44 +1,81 @@
+import 'whatwg-fetch';
+
 import { TNode } from '../types';
+import { TDatabaseRecord } from '../types';
 
 export const nodesReducer = (nodes: TNode[], action: any) => {
     if (nodes === undefined) {
         throw new Error('Initial state is undefined');
     } else {
         switch (action.type) {
-            case 'ADD_NODES':
-                console.log('in ADD_NODES');
-                // who is the parent common to all nodes from payload
-                const payloadNodes: TNode[] = action.payload;
-                const firstParentId: number = payloadNodes[0].dbrecord.child_of;
-                const sameParent = (node: TNode) => {
-                    return firstParentId === node.dbrecord.child_of;
+            case 'CHILDREN_REQUESTED':
+                console.log('in CHILDREN_REQUESTED');
+                // beware: side effects happen here:
+                const handleTheStatus = (response: Response) => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('Error when trying to retrieve the data. ' +
+                            'status text: \"' + response.statusText + '".');
+                    }
                 };
-                const payloadNodesHaveCommonParent = payloadNodes.every(sameParent);
-                if (!payloadNodesHaveCommonParent) {
-                    throw new Error('Payload nodes don\'t have one common parent');
-                }
-                // get position of parent in old state
-                const parentIndex = nodes.findIndex((node: TNode) => {
-                    return node.dbrecord.id === firstParentId;
-                });
-                if (parentIndex === -1) {
-                    return payloadNodes;
-                } else {
-                    // the new state is old state from 0 up to and including the
-                    // parent, followed by the nodes from payload, followed by
-                    // the remaining nodes from old state
-                    const begin = nodes.slice(0, parentIndex + 1);
-                    const middle = payloadNodes;
-                    const end = nodes.slice(parentIndex + 1);
-                    return begin.concat(middle).concat(end);
-                }
-            // case 'FETCH_CHILD_NODES':
-            //     // should fire a query to get child nodes of node action.payload
-            //     console.log('in FETCH_CHILD_NODES');
-            //     const node: Node = action.payload.node;
-            //     // beware: side effects happen here:
-            //     node.fetchChildNodes(action.payload.dispatch);
-            //     return state;
+                const handleTheData = (dbrecords: any) => {
+
+                    // who is the parent common to all nodes from payload
+                    const firstParentId: number = dbrecords[0].child_of;
+                    const parentIsTheSame = (dbrecord: TDatabaseRecord) => {
+                        return firstParentId === dbrecord.child_of;
+                    };
+                    const dbrecordsHaveCommonParent = dbrecords.every(parentIsTheSame);
+                    if (dbrecordsHaveCommonParent === false) {
+                        throw new Error('Not all dbrecords have the same parent.');
+                    }
+                    // get position of parent in old state
+                    const parentIndex = nodes.findIndex((node: TNode) => {
+                        return node.dbrecord.id === firstParentId;
+                    });
+
+                    // wrap the database records as TNodes
+                    const nodesToBeAdded: TNode[] = [];
+                    const wrapRecordsAsNodes = (dbrecord: TDatabaseRecord) => {
+                        nodesToBeAdded.push({
+                            dbrecord,
+                            isexpanded: false
+                        });
+                    };
+                    dbrecords.map(wrapRecordsAsNodes);
+
+                    if (parentIndex === -1) {
+                        // overwrite nodes with the list of wrapped database records
+                        nodes = nodesToBeAdded;
+                    } else {
+                        // the new state is old state from 0 up to and including
+                        // the parent, followed by the nodes that we fetch from
+                        // the database, followed by the remaining nodes from 
+                        // old state.
+                        const begin = nodes.slice(0, parentIndex + 1);
+                        const middle = nodesToBeAdded;
+                        const end = nodes.slice(parentIndex + 1);
+                        nodes = begin.concat(middle).concat(end);
+                        console.log(nodes);
+                    }
+                };
+                const handleAnyErrors = (err: Error) => {
+                    throw new Error('Errors occured.' + err.message + err.stack);
+                };
+
+                const id = action.payload;
+                const url: string = 'http://localhost:5000/node/' + id.toString() + '/children';
+
+                fetch(url, {method: 'get'})
+                        .then(handleTheStatus)
+                        .then(handleTheData)
+                        .catch(handleAnyErrors);
+
+                console.error('FIXME this doesn\'t work because it returns ' +
+                    'before nodes has been updated with the new data');
+                return nodes;
+
             case 'EXPAND_BUTTON_WAS_CLICKED':
                 console.log('in EXPAND_BUTTON_WAS_CLICKED');
                 return nodes.map((node: TNode) => {
