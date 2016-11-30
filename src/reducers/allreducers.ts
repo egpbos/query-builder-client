0d4fdc92568d66ca90dc6f7957dfd7bb6608e6f4
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-11-30 18:45:36 +0100

    need initstate at toplevel after all (see issue #16)

--- src/reducers/allreducers.ts
+++ src/reducers/allreducers.ts
@@ -1,7 +1,11 @@
 import { nodesReducer } from '../reducers';
 import { TStore }       from '../types';
 
-export const allreducers = (state: TStore, action: any) => {
+const initstate: TStore = {
+    nodes: []
+};
+
+export const allreducers = (state: TStore = initstate, action: any) => {
     return {
         nodes: nodesReducer(state.nodes, action)
     };
