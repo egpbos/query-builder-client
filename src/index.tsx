5b000f69414067691d8d369cff6b964ee73f4899
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-12-21 15:04:03 +0100

    first nodeID and key passed to <Node /> is now 1 instead of 2

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import { store }       from './store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <Node key={2} nodeID={2} />
+        <Node key={1} nodeID={1} />
     </Provider>,
     document.getElementById('root')
 );
